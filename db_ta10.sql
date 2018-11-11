14f1c4262942a557caf637f1d9eb418d959a2138
Desi Julyanti Manurung <42663004+DesiManurung@users.noreply.github.com>
2018-11-11 22:55:05 +0700

    Add files via upload

---
+++ db_ta10.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 12, 2018 at 03:15 AM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 5.6.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_ta10`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` int(13) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `angkatan` varchar(25) NOT NULL,
+  `fakultas` varchar(25) NOT NULL,
+  `program` varchar(25) NOT NULL,
+  `genre` varchar(20) NOT NULL,
+  `wisata` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`nim`, `nama`, `angkatan`, `fakultas`, `program`, `genre`, `wisata`) VALUES
+(123, 'eewfew', '2', 'fhfh', 'fhat', 'Thriller', 'Raja ampat'),
+(456, 'Cinta', '2016', 'FIT', 'Sistem Informasi', 'Thriller', 'Bangka belitung'),
+(371999, 'Desi Imut wwkwkwk', '2017', 'FIT', 'Manajemen Informatika', 'Action', 'Bali'),
+(789876, 'Manurung', '2017', 'FIT', 'Komputerisasi Akuntasi', 'Anime', 'Bali');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`nim`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `nim` int(13) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=789877;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
